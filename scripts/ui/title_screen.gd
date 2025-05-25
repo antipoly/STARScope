b91f9002e0008a11ca84a7ea2289d61a62382d9f
antipoly <80296087+antipoly@users.noreply.github.com>
2025-05-24 22:11:54 -0500

    feat: initial title screen page navigation

--- scripts/ui/title_screen.gd
+++ scripts/ui/title_screen.gd
@@ -1,16 +1,19 @@
 extends HBoxContainer
 
-@onready var PlayerName: Label = $PlayerPanel/MarginContainer/VBoxContainer/VBoxContainer/PlayerName
-@onready var SpecialisationLevel: Label = $PlayerPanel/MarginContainer/VBoxContainer/VBoxContainer/VBoxContainer/SpecialisationLevel
-@onready var FacilityLevel: Label = $PlayerPanel/MarginContainer/VBoxContainer/VBoxContainer/VBoxContainer/FacilityLevel
+@onready var player_name: Label = $PlayerPanel/MarginContainer/VBoxContainer/VBoxContainer/PlayerName
+@onready var specialisation_level: Label = $PlayerPanel/MarginContainer/VBoxContainer/VBoxContainer/VBoxContainer/SpecialisationLevel
+@onready var facility_level: Label = $PlayerPanel/MarginContainer/VBoxContainer/VBoxContainer/VBoxContainer/FacilityLevel
+
+@onready var menu_content = $"../../../";
+var duty_desk := preload("res://scenes/ui/menu/duty_desk.tscn");
 
 func _ready() -> void:
   var player = ResourceManager.get_player(false);
   if !player:
-    return ;
+    return;
 
-  PlayerName.text = player.player_name;
-  FacilityLevel.text = "Facility Level %d" % player.facility_level;
+  player_name.text = player.player_name;
+  facility_level.text = "Facility Level %d" % player.facility_level;
 
   var specialisations = ResourceManager.load_json("res://data/game/specialisations.json");
   var spec = null;
@@ -23,11 +26,10 @@ func _ready() -> void:
   if level == null:
     level = { "name": "N/A", "threshold": 0 }
 
-  SpecialisationLevel.text = "%s %s" % [spec.acr, level.name];
+  specialisation_level.text = "%s %s" % [spec.acr, level.name];
 
 func _on_duty_desk_pressed() -> void:
-  pass # Replace with function body.
-
+  change_menu_screen(duty_desk);
 
 func _on_multiplayer_pressed() -> void:
   pass # Replace with function body.
@@ -43,3 +45,15 @@ func _on_stats_pressed() -> void:
 
 func _on_help_pressed() -> void:
   pass # Replace with function body.
+
+func change_menu_screen(scene: PackedScene) -> void:
+  # Removes any other loaded scene
+  for child in menu_content.get_children():
+    if child is Control and child.scene_file_path != "":
+      child.queue_free()
+
+  
+  var instance = scene.instantiate();
+  menu_content.add_child(instance);
+
+  # Todo: Fire signal to update and animate the topbar
